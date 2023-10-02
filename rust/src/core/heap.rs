51b02d8464e1ccc263b57e49ce96755151a666ad
Victor Maia <victor.taelin@gmail.com>
2023-10-02 18:03:34 -0300

    wip

--- rust/src/core/heap.rs
+++ rust/src/core/heap.rs
@@ -1,5 +1,7 @@
 use crate::core::{*};
 
+const HEAP_SIZE : usize = 1 << 28;
+
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct Heap {
   data: Vec<(Ptr,Ptr)>,
@@ -10,8 +12,8 @@ pub struct Heap {
 impl Heap {
   pub fn new(size: usize) -> Heap {
     return Heap {
-      data: vec![(NULL,NULL); size],
-      next: 1,
+      data: vec![(NULL,NULL)],
+      next: 0,
       used: 0,
     };
   }
@@ -21,21 +23,29 @@ impl Heap {
     if size == 0 {
       return 0;
     } else {
-      let mut space = 0;
-      loop {
-        if self.next >= self.data.len() {
-          space = 0;
-          self.next = 1;
-        }
-        if self.get(self.next as Val, P1).is_nil() {
-          space += 1;
-        } else {
-          space = 0;
-        }
-        self.next += 1;
-        if space == size {
-          self.used += size;
-          return (self.next - space) as Val;
+      if self.data.len() as usize + size <= HEAP_SIZE {
+        self.data.resize(self.data.len() + size, (NULL,NULL));
+        println!("alloc {} at {}", size, self.data.len() - size);
+        return (self.data.len() - size) as Val;
+      } else {
+        let mut space = 0;
+        loop {
+          if self.next >= self.data.len() {
+            space = 0;
+            self.next = 1;
+          }
+          let p1 = self.get(self.next as Val, P1);
+          let p2 = self.get(self.next as Val, P2);
+          if p1 == NULL && p2 == NULL {
+            space += 1;
+          } else {
+            space = 0;
+          }
+          self.next += 1;
+          if space == size {
+            self.used += size;
+            return (self.next - space) as Val;
+          }
         }
       }
     }
@@ -43,9 +53,9 @@ impl Heap {
 
   #[inline(always)]
   pub fn free(&mut self, index: Val) {
-    self.used -= 1;
-    self.set(index, P1, NULL);
-    self.set(index, P2, NULL);
+    //self.used -= 1;
+    //self.set(index, P1, NULL);
+    //self.set(index, P2, NULL);
   }
 
   #[inline(always)]
