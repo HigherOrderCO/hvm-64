7a16bb5acfedb3cea64a1d45e0fcc349505f9dfe
Eduardo Henke <henke.edu@gmail.com>
2023-10-17 10:00:22 -0300

    Use separate files for benchmarks

--- benches/hvm_cpu.rs
+++ benches/hvm_cpu.rs
@@ -1,82 +1,59 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use hvm_core::{book, define, name_to_val, Net};
+use hvmc::ast::*;
+use hvmc::*;
+use std::fs;
+
+// Load file and generate net
+fn load(file: &str, size: usize, replace: Option<(&str, &str)>) -> (run::Book, run::Net) {
+  let code = fs::read_to_string(file).unwrap();
+  let code = if let Some((from, to)) = replace { code.replace(from, to) } else { code };
+  let book = ast::book_to_runtime(&ast::do_parse_book(&code));
+  let mut net = run::Net::new(size);
+  net.boot(name_to_val("main"));
+  return (book, net);
+}
 
 fn church_benchmark(c: &mut Criterion) {
-  // Initializes the book
-  let mut book = book::setup_book();
-
   let mut group = c.benchmark_group("church");
   for n in [2, 8, 24] {
     group.throughput(criterion::Throughput::Elements(n));
+    let (book, net) = load("./benches/programs/church_mul.hvmc", 1 << 12, Some(("{n}", &n.to_string())));
     group.bench_with_input(criterion::BenchmarkId::new("multiplication", n), &n, |b, &n| {
-      define(&mut book, "test", format!("$ root & (0 @c{n} (0 @c{n} root)) ~ @mul").as_str());
-      let mut net = Net::new(1 << 12);
-      net.boot(name_to_val("test"));
-      // *out = net;
-
       b.iter(|| black_box(net.clone().normal(&book)));
     });
-    // Church exponentiation (ex0)
-    // n=24 uses at most 1107 elements in heap
+    // Church exponentiation
+    // n=24 uses at most 1107 elements in heap(1 << 12 = 4096)
+    let (book, net) = load("./benches/programs/church_exp.hvmc", 1 << 12, Some(("{n}", &n.to_string())));
     group.bench_with_input(criterion::BenchmarkId::new("exponentiation", n), &n, |b, &n| {
-      define(&mut book, "test", format!("$ root & @c{n} ~ (0 @k{n} root)").as_str());
-      let mut net = Net::new(1 << 12);
-      net.boot(name_to_val("test"));
-
       b.iter(|| black_box(net.clone().normal(&book)));
     });
   }
 }
 
 fn tree_benchmark(c: &mut Criterion) {
-  // Initializes the book
-  let mut book = book::setup_book();
-
   let mut group = c.benchmark_group("tree");
-  // Allocates a big tree (ex1)
+  // Allocates a big tree
   for n in [4, 8, 16, 20] {
+    let (book, net) = load("./benches/programs/alloc_big_tree.hvmc", 16 << n, Some(("{n}", &n.to_string())));
     group.bench_with_input(criterion::BenchmarkId::new("allocation", n), &n, |b, &n| {
-      define(&mut book, "test", format!("$ root & @c{n} ~ (0 @g_s (0 @g_z root))").as_str());
-      let mut net = Net::new(16 << n);
-      net.boot(name_to_val("test"));
-
       b.iter(|| black_box(net.clone().normal(&book)));
     });
   }
 }
 
 fn binary_counter_benchmark(c: &mut Criterion) {
-  // Initializes the book
-  let mut book = book::setup_book();
-
   let mut group = c.benchmark_group("binary-counter");
-  // Decrements a BitString until it is zero (ex2)
+  // Decrements a BitString until it is zero
   for n in [4, 8, 16, 20] {
+    let (book, net) = load("./benches/programs/dec_bits.hvmc", 16 << n, Some(("{n}", &n.to_string())));
     group.bench_with_input(criterion::BenchmarkId::new("single", n), &n, |b, &n| {
-      let code = format!(
-        " $ root
-          & @c{n} ~ (0 @I (0 @E nie))
-          & @run ~ (0 nie root)"
-      );
-      define(&mut book, "test", code.as_str());
-      let mut net = Net::new(16 << n);
-      net.boot(name_to_val("test"));
-
       b.iter(|| black_box(net.clone().normal(&book)));
     });
   }
   // Decrements 2^N BitStrings until they reach zero (ex3)
   for n in [4, 8, 10] {
+    let (book, net) = load("./benches/programs/dec_bits_tree.hvmc", 128 << n, Some(("{n}", &n.to_string())));
     group.bench_with_input(criterion::BenchmarkId::new("many", n), &n, |b, &n| {
-      let code = format!(
-        " $ root
-          & @c{n} ~ (0 @S (0 @Z dep))
-          & @brn ~ (0 dep root)"
-      );
-      define(&mut book, "test", code.as_str());
-      let mut net = Net::new(128 << n);
-      net.boot(name_to_val("test"));
-
       b.iter(|| black_box(net.clone().normal(&book)));
     });
   }
