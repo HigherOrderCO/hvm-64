c4ee62275ea0f35f2fb482d0296e2e98f868920f
LunaAmora <luna.mberry@gmail.com>
2023-11-03 07:53:40 -0400

    Add normal_gpu to tests loaders

--- tests/loaders/mod.rs
+++ tests/loaders/mod.rs
@@ -46,3 +46,9 @@ pub fn normal(book: Book, size: usize) -> (run::Net, Net) {
 pub fn hvm_lang_normal(book: DefinitionBook, size: usize) -> (Term, DefNames, hvm_lang::RunInfo) {
   hvm_lang::run_book(book, size).unwrap()
 }
+
+#[cfg(feature = "cuda")]
+pub fn normal_gpu(book: Book) -> hvmc::cuda::host::HostNet {
+  let book = book_to_runtime(&book);
+  hvmc::cuda::host::run_on_gpu(&book, "main").unwrap()
+}
