15d67066ea1823697192eb7da0fea8a666d3491f
Victor Taelin <victor.taelin@gmail.com>
2024-02-02 18:31:13 -0300

    temporarily re-enable smart labels on lazy mode - seems necessary (why?)

--- src/run.rs
+++ src/run.rs
@@ -138,7 +138,7 @@ pub struct NetFields<'a, const LAZY: bool>
 where [(); LAZY as usize]: {
   pub tid : usize, // thread id
   pub tids: usize, // thread count
-  //pub labs: Lab, // dup labels
+  pub labs: Lab, // dup labels
   pub heap: Heap<'a, LAZY>, // nodes
   pub rdex: Vec<(Ptr,Ptr)>, // redexes
   pub locs: Vec<Loc>,
@@ -496,7 +496,7 @@ impl<'a, const LAZY: bool> NetFields<'a, LAZY> where [(); LAZY as usize]: {
     NetFields {
       tid : 0,
       tids: 1,
-      //labs: 0x1,
+      labs: 0x1,
       heap: Heap { nodes },
       rdex: vec![],
       locs: vec![0; 1 << 16],
@@ -1033,13 +1033,13 @@ impl<'a, const LAZY: bool> NetFields<'a, LAZY> where [(); LAZY as usize]: {
     if ptr.has_loc() {
       let tag = ptr.tag();
       // FIXME
-      //let lab = if LAZY && ptr.is_dup() && ptr.lab() == 0 { 
-        //self.labs += 2;
-        //self.labs
-      //} else {
-        //ptr.lab()
-      //};
-      let lab = ptr.lab();
+      let lab = if LAZY && ptr.is_dup() && ptr.lab() == 0 { 
+        self.labs += 2;
+        self.labs
+      } else {
+        ptr.lab()
+      };
+      //let lab = ptr.lab();
       let loc = *unsafe { self.locs.get_unchecked(ptr.loc() as usize) };
       return Ptr::new(tag, lab, loc)
     } else {
