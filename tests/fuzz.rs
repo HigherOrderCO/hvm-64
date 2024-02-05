5ebfee22f7626d383905701776b0d299b8b7c6b7
tjjfvi <t6@t6.fyi>
2024-02-05 09:56:08 -0500

    fix fuzz tests

--- tests/fuzz.rs
+++ tests/fuzz.rs
@@ -30,40 +30,22 @@ fn fuzz_var_link_link_var() {
     net.link_port_port(a.clone(), b.clone());
     net.link_port_port(c.clone(), d.clone());
     net.link_port_port(e.clone(), f.clone());
-    let mut n0 = net.fork(0, 2);
-    let mut n1 = net.fork(1, 2);
+    let mut nets = net.fork(2);
+    let mut n0 = nets.next().unwrap();
+    let mut n1 = nets.next().unwrap();
     fuzz.scope(|s| {
       s.spawn(|| {
         let (x, y) = fuzz.maybe_swap(b.clone(), c.clone());
         n0.link_wire_wire(x.wire(), y.wire());
-        trace!(n0.tracer, "done");
       });
       s.spawn(|| {
         let (x, y) = fuzz.maybe_swap(d.clone(), e.clone());
         n1.link_wire_wire(x.wire(), y.wire());
-        trace!(n1.tracer, "done");
       });
     });
-    let mut used = vec![];
-    for (x, y) in [(a.clone(), f.clone()), (f, a)] {
-      let mut p = Port(x.wire().loc().val().read());
-      if p != y {
-        loop {
-          used.push(p.loc());
-          let q = Port(p.loc().val().read());
-          if p == y.redirect() {
-            break;
-          }
-          if q.tag() == Tag::Red {
-            p = q;
-            continue;
-          }
-          panic!("bad link");
-        }
-      }
-    }
+    let used = assert_linked(a, f);
     for x in [b, c, d, e] {
-      if !used.contains(&x.loc()) && x.loc().val().read() != Port::FREE.0 {
+      if !used.contains(&x.addr()) && x.addr().val().read() != Port::FREE.0 {
         panic!("failed to free");
       }
     }
@@ -77,16 +59,17 @@ fn fuzz_pri_link_link_pri() {
   trace::set_hook();
   Fuzzer::default().fuzz(|fuzz| {
     unsafe { trace::_reset_traces() };
-    let p = Port::new(Tag::Ctr, 0, Loc::NULL);
-    let q = Port::new(Tag::Ctr, 1, Loc::NULL);
+    let p = Port::new(Tag::Ctr, 0, Addr::NULL);
+    let q = Port::new(Tag::Ctr, 1, Addr::NULL);
     let heap = Net::init_heap(256);
     let mut net = Net::new(&heap);
     let x = net.alloc();
     let a = Port::new_var(x.clone());
     let b = Port::new_var(x.other_half());
     net.link_port_port(a.clone(), b.clone());
-    let mut n0 = net.fork(0, 2);
-    let mut n1 = net.fork(1, 2);
+    let mut nets = net.fork(2);
+    let mut n0 = nets.next().unwrap();
+    let mut n1 = nets.next().unwrap();
     fuzz.scope(|s| {
       s.spawn(|| {
         n0.link_wire_port(a.wire(), p);
@@ -97,7 +80,7 @@ fn fuzz_pri_link_link_pri() {
     });
     assert!(n0.rdex.len() == 1 || n1.rdex.len() == 1);
     for x in [a, b] {
-      assert_eq!(x.loc().val().read(), Port::FREE.0);
+      assert_eq!(x.addr().val().read(), Port::FREE.0);
     }
   })
 }
@@ -119,8 +102,9 @@ fn fuzz_var_link_link_pri() {
     let d = Port::new_var(y.other_half());
     net.link_port_port(a.clone(), b.clone());
     net.link_port_port(c.clone(), d.clone());
-    let mut n0 = net.fork(0, 2);
-    let mut n1 = net.fork(1, 2);
+    let mut nets = net.fork(2);
+    let mut n0 = nets.next().unwrap();
+    let mut n1 = nets.next().unwrap();
     fuzz.scope(|s| {
       s.spawn(|| {
         let (x, y) = fuzz.maybe_swap(b.clone(), c.clone());
@@ -130,8 +114,9 @@ fn fuzz_var_link_link_pri() {
         n1.link_wire_port(d.wire(), Port::ERA);
       });
     });
-    let at = Port(a.loc().val().read());
+    let at = Port(a.addr().val().read());
     assert_eq!(at, Port::ERA);
+    // TODO: reenable leak detection
     // for x in [b, c, d] {
     //   assert_eq!(Port(x.loc().val().read()), Port::FREE, "failed to free {:?}", x.wire());
     // }
@@ -164,9 +149,10 @@ fn fuzz_var_link_link_link_var() {
     net.link_port_port(c.clone(), d.clone());
     net.link_port_port(e.clone(), f.clone());
     net.link_port_port(g.clone(), h.clone());
-    let mut n0 = net.fork(0, 3);
-    let mut n1 = net.fork(1, 3);
-    let mut n2 = net.fork(2, 3);
+    let mut nets = net.fork(3);
+    let mut n0 = nets.next().unwrap();
+    let mut n1 = nets.next().unwrap();
+    let mut n2 = nets.next().unwrap();
     fuzz.scope(|s| {
       s.spawn(|| {
         let (x, y) = fuzz.maybe_swap(b.clone(), c.clone());
@@ -181,23 +167,28 @@ fn fuzz_var_link_link_link_var() {
         n2.link_wire_wire(x.wire(), y.wire());
       });
     });
-    let mut used = vec![];
-    for (x, y) in [(a.clone(), h.clone()), (h, a)] {
-      let mut p = Port(x.wire().loc().val().read());
-      if p != y {
-        loop {
-          used.push(p.loc());
-          let q = Port(p.loc().val().read());
-          if p == y.redirect() {
-            break;
-          }
-          if q.tag() == Tag::Red {
-            p = q;
-            continue;
-          }
-          panic!("bad link");
+    assert_linked(a, h);
+  })
+}
+
+fn assert_linked(x: Port, y: Port) -> Vec<Addr> {
+  let mut used = vec![];
+  for (x, y) in [(x.clone(), y.clone()), (y, x)] {
+    let mut p = Port(x.wire().addr().val().read());
+    if p != y {
+      loop {
+        used.push(p.addr());
+        let q = Port(p.addr().val().read());
+        if p.tag() == Tag::Red && p.wire() == y.wire() {
+          break;
         }
+        if q.tag() == Tag::Red {
+          p = q;
+          continue;
+        }
+        panic!("bad link");
       }
     }
-  })
+  }
+  used
 }
