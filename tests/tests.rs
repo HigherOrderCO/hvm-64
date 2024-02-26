b45fe7fd8e72dfaccb08a176878c70e31a92a55f
tjjfvi <t6@t6.fyi>
2024-02-26 09:57:33 -0500

    clean up tests.rs

--- tests/tests.rs
+++ tests/tests.rs
@@ -54,12 +54,7 @@ fn test_bool_and() {
   assert_debug_snapshot!(rwts.total(), @"9");
 }
 
-#[derive(Default)]
-pub struct TestOpts {
-  _lazy: bool,
-}
-
-fn test_host(name: &str, host: Host, _opts: TestOpts) {
+fn test_host(name: &str, host: Host) {
   println!("{name}");
   let Some(entrypoint) = host.defs.get("main") else { return };
   let heap = run::Net::<Strict>::init_heap(1 << 28);
@@ -69,26 +64,22 @@ fn test_host(name: &str, host: Host, _opts: TestOpts) {
 
   assert_display_snapshot!(format!("{name}/strict"), host.readback(&net));
   assert_display_snapshot!(format!("{name}/strict_rwts"), net.rwts.total());
-
-  // Now, test lazy mode and
-  // ensure the outputs are equal.
 }
 
-fn test_path(path: &Path, opts: TestOpts) {
-  println!("Exec {path:?}");
+fn test_path(path: &Path) {
   let code = fs::read_to_string(&path).unwrap();
   let book = ast::Book::from_str(&code).unwrap();
   let host = Host::new(&book);
 
   let path = path.strip_prefix(env!("CARGO_MANIFEST_DIR")).unwrap();
 
-  test_host(path.to_str().unwrap(), host, opts);
+  test_host(path.to_str().unwrap(), host);
 }
 
-fn test_dir(dir: &Path, filter: &dyn Fn(&Path) -> bool) {
+fn test_dir(dir: &Path, filter: impl Fn(&Path) -> bool) {
   insta::glob!(dir, "**/*.hvmc", |p| {
     if filter(p) {
-      test_path(p, Default::default());
+      test_path(p);
     }
   })
 }
@@ -103,22 +94,22 @@ fn manifest_relative(sub: &str) -> PathBuf {
 
 #[test]
 fn test_fast_programs() {
-  test_dir(&manifest_relative("tests/programs/"), &|p| !is_slow(p))
+  test_dir(&manifest_relative("tests/programs/"), |p| !is_slow(p))
 }
 
 #[test]
 #[ignore = "very slow"]
 fn test_slow_programs() {
-  test_dir(&manifest_relative("tests/programs/"), &|p| is_slow(p))
+  test_dir(&manifest_relative("tests/programs/"), |p| is_slow(p))
 }
 
 #[test]
 fn test_fast_examples() {
-  test_dir(&manifest_relative("examples/"), &|p| !is_slow(p));
+  test_dir(&manifest_relative("examples/"), |p| !is_slow(p));
 }
 
 #[test]
 #[ignore = "very slow"]
 fn test_slow_examples() {
-  test_dir(&manifest_relative("examples/"), &|p| is_slow(p))
+  test_dir(&manifest_relative("examples/"), |p| is_slow(p))
 }
