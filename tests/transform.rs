021babe87c8efeb0eef0d35066185aa9e08ec50f
T6 <t6@t6.fyi>
2024-03-20 14:16:56 -0400

    rework heap allocation api (#93)

--- tests/transform.rs
+++ tests/transform.rs
@@ -12,12 +12,12 @@ pub fn test_fast_pre_reduce() {
   let book = parse_core(&load_file("heavy_pre_reduction.hvmc"));
   let (mut book_1, mut book_2) = (book.clone(), book);
 
-  let rwts_1 = book_1.pre_reduce(&|x| !["expensive", "main_fast"].contains(&x), 1 << 29, u64::MAX).rewrites;
+  let rwts_1 = book_1.pre_reduce(&|x| !["expensive", "main_fast"].contains(&x), None, u64::MAX).rewrites;
   let rwts_2 =
-    book_2.pre_reduce(&|x| !["expensive_1", "expensive_2", "main_slow"].contains(&x), 1 << 29, u64::MAX).rewrites;
+    book_2.pre_reduce(&|x| !["expensive_1", "expensive_2", "main_slow"].contains(&x), None, u64::MAX).rewrites;
 
-  let rwts_1 = show_rewrites(&(rwts_1 + normal_with(book_1, 1 << 29, "main_fast").0));
-  let rwts_2 = show_rewrites(&(rwts_2 + normal_with(book_2, 1 << 29, "main_slow").0));
+  let rwts_1 = show_rewrites(&(rwts_1 + normal_with(book_1, None, "main_fast").0));
+  let rwts_2 = show_rewrites(&(rwts_2 + normal_with(book_2, None, "main_slow").0));
 
   assert_snapshot!(format!("Fast:\n{rwts_1}Slow:\n{rwts_2}"), @r###"
   Fast:
