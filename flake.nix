5f782ea506c10a4c06bc39c9ad500065906e6264
Enrico Borba <enricozb@users.noreply.github.com>
2024-04-03 12:27:49 -0400

    add nix-dev files (#113)

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "HVM-Core";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.fenix.url = "github:nix-community/fenix/monthly";
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          # to recompute the hash, replace with `pkgs.lib.fakeSha256`
+          sha256 = "sha256-hBzihtLpwbCyL5AgwKj0sUbJXRir18utWgqUZHGsbFs=";
+        };
+      in {
+        devShells.default = pkgs.mkShell { packages = [ rust-toolchain ]; };
+      });
+}
