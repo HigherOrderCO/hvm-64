73537a2c1c8d93960aa160a6c5788e1c679a27eb
Enrico Zandomeni Borba <enricozb@gmail.com>
2024-04-03 12:12:13 -0400

    use rust-toolchain.toml

--- flake.nix
+++ flake.nix
@@ -6,14 +6,15 @@
 
   outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with fenix.packages.${system}; [
-            minimal.rustc
-            minimal.cargo
-            pkgs.clippy
-          ];
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          # to recompute the hash, replace with `pkgs.lib.fakeSha256`
+          sha256 = "sha256-hBzihtLpwbCyL5AgwKj0sUbJXRir18utWgqUZHGsbFs=";
         };
+      in {
+        devShells.default =
+          pkgs.mkShell { packages = [ toolchain pkgs.clippy ]; };
       });
 }
